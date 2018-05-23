1e60f49f48a1d7407a36eea55f2ada05347eaa56
rishidk22 <rishidk22@gmail.com>
2018-05-23 14:41:07 -0500

    Added APIKEY

--- api/app.py
+++ api/app.py
@@ -2,11 +2,19 @@ from flask import Flask, request
 import json, requests, jsonify
 
 
-app = Flask(__name__)
+APIKEY = 7ab1c5c2151720f0b4104d7a9a2d7b9f
+
+
+#----------HELLPER FUNCTIONS----------------
 
 def getJson(incomingRequest):
     requestBody =  json.loads((incomingRequest).decode("utf-8"))
     return requestBody
+
+
+#----------API FRAMEWORK/PROCCESSING-------------------
+
+app = Flask(__name__)
 
 @app.route("/")
 def home():
@@ -14,7 +22,7 @@ def home():
 
 @app.route("/home", methods = ['POST'])
 def helloWorld():
-    #return getJson(request.data).get("hello")
     return request.headers["Content-Type"]
+
 if __name__ == "__main__":
     app.run(debug = True)
