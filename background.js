af8822c6377f9415d4c230a6305a1fde452c725f
kartiktennis <krishnankartik6@gmail.com>
2018-05-24 12:18:35 -0400

    minor

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 var currentURL;
 
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, 
+
 function(tabs){
 	getCurrentURL(tabs[0].url);
 });
 
 function getCurrentURL(tab){
-	currentURL = tab;
+    currentURL = tab;
+    console.log(currentURL);
 }
 
-console.log(currentUrl);
