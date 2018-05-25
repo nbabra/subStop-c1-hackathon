a101eafc97f90c0c26b6bac96ab2bfa43dbce9ce
rishidk22 <32312825+rishidk22@users.noreply.github.com>
2018-05-25 08:24:42 -0500

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request, jsonify, Response
+from flask import Flask, request, jsonify, Response, render_template
 from flask_mail import Mail, Message
 import json, requests, os, sqlite3
 from collections import OrderedDict
@@ -10,15 +10,6 @@ import smtplib
 APIKEY = { 'key': ''}
 S3_ACCESS_KEY = ""
 S3_SECRET_KEY = ""
-
-mail_settings = {
-    "MAIL_SERVER": 'smtp@gmail.com',
-    "MAIL_PORT": 465,
-    "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": 'substop18@gmail.com',
-    "MAIL_PASSWORD": 'substop123'
-}
 
 
 #----------HELPER FUNCTIONS----------------
@@ -45,11 +36,11 @@ def createSubscription(subName, body):
 def uploadToS3Account(userName, filename):
     conn = tinys3.Connection(S3_ACCESS_KEY,S3_SECRET_KEY)
     f = open(filename,'rb')
-    conn.upload("/users/" + userName + "/" + filename,f,'substop18')
+    conn.upload("/users/" + userName + "/" + filename,f,'substopv2')
     f.close()
 
 def updateSubscription(subname, userName, body):
-    response = requests.get("https://s3-us-west-1.amazonaws.com/substop18/users/" + userName + "/" + subname.lower() + ".json")
+    response = requests.get("https://s3.us-east-2.amazonaws.com/substopv2/users/" + userName + "/" + subname.lower() + ".json")
     if(response.status_code != 200):
         print "postpajtdjfidasopjfisapdji"
         return "NULL"
@@ -59,31 +50,35 @@ def updateSubscription(subname, userName, body):
     return filename
 
 def getCustomerID(username):
-    response = requests.get("https://s3-us-west-1.amazonaws.com/substop18/users/" + username + "/ACCOUNT.json")
+    response = requests.get("https://s3.us-east-2.amazonaws.com/substopv2/users/" + username + "/ACCOUNT.json")
     temp = json.loads(response.content)
     return temp.get("customerID")
 
 def getPhoneNumber(username):
-    response = requests.get("https://s3-us-west-1.amazonaws.com/substop18/users/" + username + "/ACCOUNT.json")
+    response = requests.get("https://s3.us-east-2.amazonaws.com/substopv2/users/" + username + "/ACCOUNT.json")
     temp = json.loads(response.content)
     return temp.get("phone")
 
 #----------API FRAMEWORK/PROCCESSING-------------------
 app = Flask(__name__)
 
-app.config.update(mail_settings)
-mail = Mail(app)
-
 @app.route("/")
 def home():
-    return "WELCOME TO SUBSTOP API! DOCUMENTATION COMING SOON"
+    return render_template('login-page.html')
+
+@app.route("/thank-you")
+def load():
+    body = "{'username':'testUser', 'customerID':'5b0439bd322fa06b677939e8', 'phone':'6306052697'}"
+    data = json.loads(body)
+    response = requests.post("http://3676dca2.ngrok.io/createAccount", data)
+    return render_template('thank-you.html')
 
 
 @app.route("/setSubscriptions", methods = ['POST']) #username, body  GETS all subscriptions
 def setSubscriptions():
-    username = getJSON(request.data).get("username")
+    username = json.loads(request.data).get("username")
     ###CONFIGURE BODY
-    body = getJSON(request.data)
+    body = json.loads(request.data)
     del body['username']
     for item in getBills(getCustomerID(username)):
         print item
@@ -95,8 +90,10 @@ def setSubscriptions():
 
 @app.route("/createAccount", methods = ['POST'])
 def createAccount():
-    username = getJSON(request.data).get("username")
-    body =  getJSON(request.data)
+    print request.data
+    username = json.loads(request.data).get("username")
+    body =  json.loads(request.data)
+    print request.data
     del body['username']
     with open("ACCOUNT.json","w") as f:
         json.dump(body, f)
@@ -107,22 +104,22 @@ def createAccount():
 @app.route("/updateSubscription", methods = ['POST']) #username, subname, body(date and phone number whatever else)
 def updateSubscriptions():
     print request.data
-    username = getJSON(request.data).get("username")
-    subname = getJSON(request.data).get("subname")
+    username = json.loads(request.data).get("username")
+    subname = json.loads(request.data).get("subname")
     subname = subname.lower()
     body = getJSON(request.data)
     del body['username']
     del body['subname']
     filename = updateSubscription(subname, username, body)
     if(filename=="NULL"):
-        return Response(status = 400)
+        return Response(status = 200)
     uploadToS3Account(username, filename)
     os.remove(filename)
     return Response(status = 200)
 
 @app.route("/checkSubscription/<username>/<subname>") #username, subname #returns date
 def checkSubscription(username, subname):
-    response = requests.get("https://s3-us-west-1.amazonaws.com/substop18/users/" + username + "/" +subname.lower() + ".json")
+    response = requests.get("https://s3.us-east-2.amazonaws.com/substopv2/users/" + username + "/" +subname.lower() + ".json")
     return response.json().get("date")
 
 @app.route("/sendText", methods = ['POST'])
